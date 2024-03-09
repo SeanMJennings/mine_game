53223b4a1fda2491fd3e63d90ab510f0996d929e
CuriousConsistency <seanjennings976@gmail.com>
2024-03-09 11:26:28 +0000

    wip

--- src/domain/board.py
+++ src/domain/board.py
@@ -1,9 +1,18 @@
+from enum import Enum
 from .player import Player
 
 class Board:
   def __init__(self):
     self.player = Player()
   def move_player(self, direction):
-    self.player.y = 1
+    match direction.lower():
+      case Direction.Up:
+        self.player.y += 1      
+      case Direction.Down:
+        self.player.y -= 1
   def get_player(self):
     return self.player
+  
+class Direction(Enum):
+  Up = 'up',
+  Down = 'down',
--- testing/domain/board_test.py
+++ testing/domain/board_test.py
@@ -7,3 +7,14 @@ def test_move_player_up():
     position = board.get_player()
     assert position.x == 0
     assert position.y == 1
+    
+def test_move_player_down():
+    board = Board()
+    board.move_player('Up')
+    board.move_player('Down')
+    position = board.get_player()
+    assert position.x == 0
+    assert position.y == 0
+
+if __name__ == "__main__":
+    pytest.main(["board_test.py", "-s"])
