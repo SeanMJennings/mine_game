a10b53b623e3ff7a59284c0a7456486fd25d685e
CuriousConsistency <seanjennings976@gmail.com>
2024-03-10 21:46:42 +0000

    wip

--- src/domain/board.py
+++ src/domain/board.py
@@ -2,18 +2,24 @@ from enum import Enum
 from .player import Player
 
 class Direction(Enum):
-  UP = 'up',
-  DOWN = 'down',
+  up = 'up',
+  down = 'down',
+  left = 'left',
+  right = 'right'
 
 class Board:
   def __init__(self, player : Player):
     self._player = player
   def move_player(self, direction : Direction):
-    direction = Direction[direction.upper()]
+    direction = Direction[direction.lower()]
     match direction:
-      case Direction.UP:
+      case Direction.up:
         self._player.y += 1      
-      case Direction.DOWN:
-        self._player.y -= 1
+      case Direction.down:
+        self._player.y -= 1      
+      case Direction.left:
+        self._player.x -= 1      
+      case Direction.right:
+        self._player.x += 1
   def get_player(self):
     return self._player
